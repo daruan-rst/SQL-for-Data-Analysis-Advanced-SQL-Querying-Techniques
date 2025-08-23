e53de6cafc476d68c63f2b1ab6ef051c0eabadc9
Daruan <daruan.rst@gmail.com>
2025-08-22 22:06:31 -0300

    28. Joining Multiple Tables

--- 2 - Multi-Table Analysis/Multi-Table-Analysis.sql
+++ 2 - Multi-Table Analysis/Multi-Table-Analysis.sql
@@ -124,6 +124,21 @@ AND hs.`year` = ir.`year`
 
 -- 4. Joinning multiple tables
 
+SELECT * FROM happiness_scores hs;
+
+SELECT * FROM country_stats cs;
+
+SELECT * FROM inflation_rates ir;
+
+SELECT hs.`year` , hs.country , hs.happiness_score , 
+		cs.continent, ir.inflation_rate 
+FROM happiness_scores hs 
+LEFT JOIN country_stats cs  
+ON hs.country = cs.country 
+LEFT JOIN inflation_rates ir 
+ON hs.`year` = ir.`year` 
+AND hs.country  = ir.country_name;
+
 -- 5. Self joins
 
         
