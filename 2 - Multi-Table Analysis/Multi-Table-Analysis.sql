4961e31984e6df654735c39e3feb497d8c806fad
Daruan <daruan.rst@gmail.com>
2025-08-28 23:44:15 -0300

    32. Cross Joins

--- 2 - Multi-Table Analysis/Multi-Table-Analysis.sql
+++ 2 - Multi-Table Analysis/Multi-Table-Analysis.sql
@@ -191,6 +191,54 @@ ON e1.manager_id = e2.employee_id;
 
  
 -- 6. Cross joins
+CREATE TABLE tops (
+    id INT,
+    item VARCHAR(50)
+);
 
+
+CREATE TABLE sizes (
+    id INT,
+    size VARCHAR(50)
+);
+
+CREATE TABLE outerwear (
+    id INT,
+    item VARCHAR(50)
+);
+
+INSERT INTO tops (id, item) VALUES
+	(1, 'T-Shirt'),
+	(2, 'Hoodie');
+
+INSERT INTO sizes (id, size) VALUES
+	(101, 'Small'),
+	(102, 'Medium'),
+	(103, 'Large');
+
+INSERT INTO outerwear (id, item) VALUES
+	(2, 'Hoodie'),
+	(3, 'Jacket'),
+	(4, 'Coat');
+
+-- 6.1 View the tables
+SELECT * FROM tops;
+SELECT * FROM sizes;
+SELECT * FROM outerwear;
+
+-- 6.2 Cross join the tables
+SELECT *
+FROM tops t CROSS JOIN sizes s ;
+
+-- 6.3 Assingment 2 (Self Join) rewritten with a CROSS JOIN
+
+SELECT	p1.product_name, p1.unit_price,
+		p2.product_name, p2.unit_price,
+        p1.unit_price - p2.unit_price AS price_diff
+FROM	products p1 
+CROSS JOIN products p2
+WHERE	ABS(p1.unit_price - p2.unit_price) < 0.25
+AND p1.product_name < p2.product_name
+ORDER BY price_diff DESC;
     
 -- 7. Union vs union all
