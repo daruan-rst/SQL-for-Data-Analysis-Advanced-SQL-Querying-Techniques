70f729a4a4a623a7b646ea507677cd73dfcfd497
Daruan <daruan.rst@gmail.com>
2025-09-07 12:39:37 -0300

    40. Subqueries in the FROM Clause

--- 3 - Subqueries & CTEs/Subqueries-and-CTEs.sql
+++ 3 - Subqueries & CTEs/Subqueries-and-CTEs.sql
@@ -14,7 +14,25 @@ SELECT	year, country, happiness_score,
 FROM	happiness_scores;
 
 -- 2. Subqueries in the FROM clause
+SELECT * FROM happiness_scores hs 
 
+ --  Average happiness score for each country
+
+SELECT hs.country, AVG(hs.happiness_score) as avg_hs_by_country
+FROM happiness_scores hs 
+GROUP BY hs.country 
+
+/* Return a country's happiness score for the year as well as
+ the average happiness score for the country across the years */
+
+SELECT hs.year, hs.country, hs.happiness_score,
+		country_hs.avg_hs_by_country
+FROM happiness_scores hs 
+LEFT JOIN (SELECT hs.country, AVG(hs.happiness_score) as avg_hs_by_country
+			FROM happiness_scores hs 
+			GROUP BY hs.country ) AS country_hs
+ON country_hs.country = hs.country
+WHERE hs.country = 'United States';
             
 -- 3. Multiple subqueries
 
